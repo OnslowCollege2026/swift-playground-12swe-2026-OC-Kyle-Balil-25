1953c213c4880cfea8e3b27ce3435434a45261e0
OC-Kyle-Balil-25 <kyle.balili@student.onslow.school.nz>
2026-02-11 21:28:28 +0000

    Day 1-2: Revision Task (Dupe)

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,35 +1,57 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+func readNumber(prompt: String) -> Double {
+    print(prompt)
+    var returnValue = -1.0
+
+    // User Input for Room Dimensions
+    guard let userInput = readLine(), let number = Double(userInput), number >= 0 else {
+        print ("Invalid input, please enter a positive number with no more than 1 decimal place")
+        return readNumber(prompt: prompt)
+    }
+    returnValue = number
+    return returnValue
+}
+
 @main
 struct SwiftPlayground {
     static func main() {
         print("Hello, world!")
 
-        // 5/02/2025 //
+        // Day 0: Introductory Spell
 
+        //// 5&10/02/2025 ////
+        /// Day 1&2: Recap Task
 
-    // Prevents Crash Error
-    var lengthRoom = 0
-    var widthRoom = 0
-    var heightRoom = 0
+        // Magic Number Variable
+        var furnitureVolumeMax = 2.0
+        // Variables that *should* export the values from the loop to outside the loop
+        var roomLength = readNumber(prompt: "Please enter the room length in m²")
+        var roomWidth = readNumber(prompt: "Please enter the room width in m²")
+        var roomHeight = readNumber(prompt: "Please enter the room height in m²")
 
-    // User inputs dimension data for calculating room volume
-    print("Enter room length:")
-    if let lengthInput = readLine(), var length = Double(lengthInput), var lengthRoom = length {
-    }
-    print("Enter room width:")
-    if let widthInput = readLine(), var width = Double(widthInput) {
-    }
-    print("Enter room height:")
-    if let heightInput = readLine(), var height = Double(heightInput) {
-    }
-    
-    // Basic Intermediate Mathing
-    let roomArea = lengthRoom * widthRoom
-    let roomVolume = lengthRoom * widthRoom * heightRoom
-    print ("The floor is \(roomArea)m² big!")
-    print ("The room is \(roomVolume)m² big!")
-    }
+        let floorArea = roomLength * roomWidth
+        print ("Room Area: \(floorArea)m²")
+        let roomVolume = floorArea * roomHeight
+        print ("Room Volume: \(roomVolume)m³")
+        
+        // Furniture Process Starts
+        var furnitureVolumes = [1.2, 0.0, 2.5, 0.6, 1.0]
+        var totalFurnitureVolume = 0.0
+
+        furnitureVolumes.enumerated().forEach { furniture, singleFurnitureVolume in
+            print("Item \(furniture + 1): \(singleFurnitureVolume)m³")
+            if singleFurnitureVolume > furnitureVolumeMax {
+                print("Oversized item detected.")
+            }
+            totalFurnitureVolume += singleFurnitureVolume
+        }
+
+        var usableVolume = roomVolume - totalFurnitureVolume
+        print ("Volume in room remaining: \(usableVolume)m³")
+
+        //// 5&10/02/2025 ////
+        /// Day 3: Functions Without Parametres
+        }
 }
-
