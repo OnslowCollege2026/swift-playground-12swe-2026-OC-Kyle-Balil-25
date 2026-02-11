ab8fd83a1204ad71e544c196068d50b9d71c921f
OC-Kyle-Balil-25 <kyle.balili@student.onslow.school.nz>
2026-02-11 21:22:01 +0000

    Day 1-2: Revision Task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,5 +5,31 @@
 struct SwiftPlayground {
     static func main() {
         print("Hello, world!")
+
+        // 5/02/2025 //
+
+
+    // Prevents Crash Error
+    var lengthRoom = 0
+    var widthRoom = 0
+    var heightRoom = 0
+
+    // User inputs dimension data for calculating room volume
+    print("Enter room length:")
+    if let lengthInput = readLine(), var length = Double(lengthInput), var lengthRoom = length {
+    }
+    print("Enter room width:")
+    if let widthInput = readLine(), var width = Double(widthInput) {
+    }
+    print("Enter room height:")
+    if let heightInput = readLine(), var height = Double(heightInput) {
+    }
+    
+    // Basic Intermediate Mathing
+    let roomArea = lengthRoom * widthRoom
+    let roomVolume = lengthRoom * widthRoom * heightRoom
+    print ("The floor is \(roomArea)m² big!")
+    print ("The room is \(roomVolume)m² big!")
     }
 }
+
